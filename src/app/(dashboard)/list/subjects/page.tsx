f23cc8870143ceb21810a8fa04a1d166b9872d4e
Siesta1510 <93849920+Siesta1510@users.noreply.github.com>
2024-12-10 00:58:51 +0700

    feat: add react-toastify for notifications and implement SubjectForm for creating/updating subjects

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,3 +1,4 @@
+import FormContainer from "@/components/FormContainer";
 import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
@@ -45,8 +46,7 @@ const SubjectListPage = async ({
         <div className="flex items-center gap-2">
           {role === "admin" && (
             <>
-              <FormModal table="subject" type="update" data={item} />
-              <FormModal table="subject" type="delete" id={item.id} />
+              <FormContainer table="subject" type="create" />
             </>
           )}
         </div>
---
+++ src/lib/actions.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import {
+  ClassSchema,
+  ExamSchema,
+  StudentSchema,
+  SubjectSchema,
+  TeacherSchema,
+} from "./formValidationSchemas";
+import prisma from "./prisma";
+import { clerkClient } from "@clerk/nextjs/server";
+
+type CurrentState = { success: boolean; error: boolean };
+
+export const createSubject = async (
+  currentState: CurrentState,
+  data: SubjectSchema
+) => {
+  try {
+    await prisma.subject.create({
+      data: {
+        name: data.name,
+        teachers: {
+          connect: data.teachers.map((teacherId) => ({ id: teacherId })),
+        },
+      },
+    });
+
+    // revalidatePath("/list/subjects");
+    return { success: true, error: false };
+  } catch (err) {
+    console.log(err);
+    return { success: false, error: true };
+  }
+};
+
+export const updateSubject = async (
+    currentState: CurrentState,
+    data: SubjectSchema
+  ) => {
+    try {
+      await prisma.subject.update({
+        where: {
+          id: data.id,
+        },
+        data: {
+          name: data.name,
+          teachers: {
+            set: data.teachers.map((teacherId) => ({ id: teacherId })),
+          },
+        },
+      });
+  
+      // revalidatePath("/list/subjects");
+      return { success: true, error: false };
+    } catch (err) {
+      console.log(err);
+      return { success: false, error: true };
+    }
+  };
